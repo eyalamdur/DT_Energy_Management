b8da46fe40b6577c08a4c76e24fb6a49f7b8abd1
eyalamdur <eyal.amdur@campus.technion.ac.il>
2025-04-29 16:43:07 +0300

    ANM6Easy working env

---
+++ code/envs/anm/anm_tests.py
@@ -0,0 +1,81 @@
+import gymnasium as gym
+import gym_anm
+import numpy as np
+
+def create_environment(env_name : str, entry_point : str) -> gym.Env:
+    """
+    Create a gym environment with the specified name and maximum episode steps.
+    Args:
+        env_name (str): The name of the environment to create.
+        entry_point (str): The entry point for the environment.
+    Returns:
+        env (gym.Env): The created gym environment.
+    """
+    
+    # Register the environment if it is not already registered
+    if env_name not in gym.envs.registry:
+        print(f"Registering environment '{env_name}'.")
+        gym.register(id=env_name,entry_point=entry_point)
+        
+    # Create and return the environment
+    env = gym.make(env_name)
+    return env
+
+def collect_trajectories(env : gym.Env, num_episodes : int = 500, min_traj_length : int = 20) -> list:
+    """
+    Collect trajectories from the specified environment.
+    Args:
+        env_name (str): The name of the environment to collect trajectories from.
+        num_episodes (int): The number of episodes to collect.
+    Returns:
+        trajectories (list): A list of collected trajectories.
+    """
+
+    # Collect trajectories
+    trajectories = []
+    for _ in range(num_episodes):
+        print(f"Collecting episode {_ + 1}/{num_episodes}")
+        obs, _ = env.reset()
+        done = False
+        states, actions, rewards = [], [], []
+
+        while not done:
+            action = env.action_space.sample()
+            next_obs, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+            states.append(obs)
+            actions.append(action)
+            rewards.append(reward)
+
+            obs = next_obs
+
+        # filter out short episodes
+        if len(states) >= min_traj_length:
+            rtgs = np.cumsum(rewards[::-1])[::-1]
+            trajectories.append({
+                "states": np.array(states),
+                "actions": np.array(actions),
+                "rtgs": rtgs
+            })
+    return trajectories
+
+
+def main():
+    # Create the environment
+    env = create_environment(env_name='ANM6Easy-v0', entry_point='gym_anm.envs.anm6_env.anm6_easy:ANM6Easy')
+    print("Environment created successfully.")
+    
+    # Collect trajectories
+    trajectories = collect_trajectories(env, num_episodes=20, min_traj_length=1)
+    print(f"Collected {len(trajectories)} trajectories.")
+    for i, traj in enumerate(trajectories):
+        print(f"Trajectory {i + 1}:")
+        print(f"States: {traj['states']}")
+        print(f"Actions: {traj['actions']}")
+        print(f"RTGs: {traj['rtgs']}")
+        print()
+        
+if __name__ == "__main__":
+    main()
+

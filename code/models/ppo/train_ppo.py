adb9d6679e57e79c60660e470816b5d6ec7326de
eyalamdur <eyal.amdur@campus.technion.ac.il>
2025-05-28 17:32:49 +0300

    PPO & TD3 agents

--- code/models/ppo/train_ppo.py
+++ code/models/ppo/train_ppo.py
@@ -1,32 +1,29 @@
 import gymnasium as gym
-from models.anm_exp.rl_agents.train import PPOAgent 
+from stable_baselines3 import PPO
 
-def train_ppo(env :gym.Env, num_episodes: int = 1000) -> PPOAgent:
+def train_ppo(env: gym.Env, num_episodes: int = 1000) -> PPO:
     """
     Train a PPO agent on the given environment.
     Args:
         env (gym.Env): The environment to train the agent on.
         num_episodes (int): The number of episodes to train the agent.
     Returns:
-        agent (PPOAgent): The trained PPO agent.
+        agent (PPO): The trained PPO agent.
     """
     # Create the PPO agent
-    agent = PPOAgent(env)
-
-    # Training loop
-    for episode in range(num_episodes):
-        done = False
-        obs = env.reset()
-        total_reward = 0
-
-        while not done:
-            action = agent.select_action(obs)
-            obs, reward, done, info = env.step(action)
-            agent.store_transition(obs, action, reward, done)
-            total_reward += reward
-
-        agent.train()
-
-        print(f"Episode {episode+1} finished with total reward: {total_reward}")
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=num_episodes)
+    model.save("code/models/ppo/ppo_anm6easy")
     
-    return agent
+    return model
+
+def load_ppo(model_path: str) -> PPO:
+    """
+    Load a trained PPO agent from a file.
+    Args:
+        model_path (str): The path to the saved PPO model.
+    Returns:
+        agent (PPO): The loaded PPO agent.
+    """
+    model = PPO.load(model_path)
+    return model
--- code/models/train_models.py
+++ code/models/train_models.py
@@ -1,14 +1,36 @@
 import decision_transformer.utils as utils
 import models.ppo.train_ppo as ppo
+import models.td3.train_td3 as td3
+import utils 
+
 
 def main():
+    """
+    Main function to create the environment and train or load the agents.
+    This function initializes the environment, checks if the models are available,
+    and trains or loads the PPO and TD3 agents accordingly.
+    """
     # Create the environment
     env = utils.create_environment(env_name='gym_anm:ANM6Easy-v0', entry_point='gym_anm.envs.anm6_env.anm6_easy:ANM6Easy')
-    print("Environment created successfully.")
-        
-    # train the PPO agent
-    ppo_agent = ppo.train_ppo(env)
-    print("PPO training completed.")
+    utils.color_print("Environment created successfully.")
 
+    # train/load the PPO agent
+    if (not utils.is_model_available("code/models/ppo/ppo_anm6easy")):
+        # Train the PPO agent
+        ppo_agent = ppo.train_ppo(env)
+    else:
+        # Load the PPO agent
+        ppo_agent = ppo.load_ppo("code/models/ppo/ppo_anm6easy")
+    print("PPO agent is ready.")
+
+    # train/load the TD3 agent
+    if (not utils.is_model_available("code/models/td3/td3_anm6easy")):
+        # Train the TD3 agent
+        td3_agent = td3.train_td3(env)
+    else:
+        # Load the TD3 agent
+        td3_agent = td3.load_td3("code/models/td3/td3_anm6easy")
+    print("TD3 agent is ready.")
+    
 if __name__ == "__main__":
     main()
